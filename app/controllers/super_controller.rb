4b61154571ceb799ef57600039a0080f5e2b1b3a
ykazuki <kazuki.yamaguchi@theport.jp>
2015-12-11 20:25:11 +0900

    css 修正

--- app/controllers/super_controller.rb
+++ app/controllers/super_controller.rb
@@ -34,7 +34,7 @@ class SuperController < ApplicationController
   #
   def dashboard
     @AllProject = Project.all.count
-    #@NoFinishTask = Task
+    @NoFinishTask = Task.where(:deleted_at => nil,:state => 0).count
     now_time = DateTime.now
     @events = Event.where(Event.arel_table[:start].lt(now_time).and(Event.arel_table[:end].gt(now_time)).and(Event.arel_table[:deleted_at].eq(nil))).all
   end
